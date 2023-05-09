957db5f009d6448831d03a669502b9d7c3337e8f
AbeArendsUNC <arends@ad.unc.edu>
2023-05-09 04:25:24 -0400

    3

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -35,5 +35,22 @@ let choice = args._[0]
 try {
     console.log(JSON.stringify(rps(shoot)));
 } catch(error) {
+    console.log( `Usage: node-rps [SHOT]
+    Play Rock Paper Scissors (RPS)
+    
+      -h, --help      display this help message and exit
+      -r, --rules     display the rules and exit
+    
+    Examples:
+      node-rps        Return JSON with single player RPS result.
+                      e.g. {"player":"rock"}
+      node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                      e.g {"player":"rock","opponent":"scissors","result":"win"}`
+    );
+    console.log(`Rules for Rock Paper Scissors:
+
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock CRUSHES Scissors`);
     process.exit(1);
 }
